1f30dcfefa63378ab207700e2c047193556ecd9b
Kjeld Flindt Ejlerskov <kjel0737@skivecollege.dk>
2019-08-05 22:56:11 +0200

    Conected to db

---
+++ StatestikAspITPlannerKjeld/Helpers/DB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatestikAspITPlannerKjeld.Helpers
+{
+    public class DB
+    {
+        private static readonly string conString = ConfigurationManager.ConnectionStrings["DBCon"].ConnectionString;
+
+
+        public void getAll()
+        {
+            List<int> ids = new List<int>();
+            SqlConnection cnn = new SqlConnection(conString);
+            
+            cnn.Open();
+
+            SqlCommand cmd;
+            String sql = "SELECT * FROM Presents p, Students s where p.StudentID = s.ID ";
+            
+            cmd = new SqlCommand(sql, cnn);
+            //cmd.Parameters.AddWithValue("StudentID", 7);
+            using (SqlDataReader oReader = cmd.ExecuteReader())
+            {
+                while (oReader.Read())
+                {
+                     int id = int.Parse(oReader["StudentID"].ToString());
+                     string name = oReader["Name"].ToString();
+                     ids.Add(id);
+                }
+
+            }
+        }
+    }
+}
--- StatestikAspITPlannerKjeld/MainWindow.xaml.cs
+++ StatestikAspITPlannerKjeld/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StatestikAspITPlannerKjeld.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace StatestikAspITPlannerKjeld
         public MainWindow()
         {
             InitializeComponent();
+            DB db = new DB();
+            db.getAll();
         }
     }
 }
